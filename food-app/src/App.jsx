c77ce0d0f05f06125b79e6aac73a7f649e34c52d
Michael Uzoigwe <michaeluzoigwe78@gmail.com>
2025-10-21 08:48:22 +0100

    Complete food app using react js and tailwind css

---
+++ food-app/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import HeadlineCards from "./components/HeadlineCards"
+import Food from "./components/Food"
+import Category from "./components/Category"
+
+
+function App() {
+
+  return (
+    <>
+     <Navbar />
+     <Hero />
+     <HeadlineCards />
+     <Food />
+     <Category />
+    </>
+  )
+}
+
+export default App
