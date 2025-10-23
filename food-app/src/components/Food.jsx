5e58ea4a1fc286a766331639622f837fe52b453d
Michael Uzoigwe <michaeluzoigwe78@gmail.com>
2025-10-23 01:45:19 +0100

    updated food file

--- food-app/src/components/Food.jsx
+++ food-app/src/components/Food.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { data } from '../data/data'
+import React, { useState } from 'react';
+import { data } from '../data/data';
 
 const Food = () => {
   // console.log(data)
